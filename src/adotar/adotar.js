8a025752d1e629338b26bc876a3d22639a7aa2da
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2023-05-28 00:23:16 -0300

    added primeiras interações com js na página

--- src/adotar/adotar.js
+++ src/adotar/adotar.js
@@ -1,9 +1,149 @@
-function irParaPet() {
+let pets = [
+  {
+    id: "01",
+    name: "Gilmar",
+    img: "/src/adotar/img/petGilmar.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 3,
+    adotado: false,
+    sexo: "male",
+    tipo: "cat",
+  },
+  {
+    id: "02",
+    name: "Lucas",
+    img: "/src/adotar/img/petLucas.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 6,
+    adotado: false,
+    sexo: "male",
+    tipo: "dog",
+  },
+
+  {
+    id: "03",
+    name: "Mari",
+    img: "/src/adotar/img/petMari.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 1,
+    adotado: false,
+    sexo: "male",
+    tipo: "cat",
+  },
+  {
+    id: "04",
+    name: "Gilmar",
+    img: "/src/adotar/img/petGilmar.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 3,
+    adotado: false,
+    sexo: "male",
+    tipo: "cat",
+  },
+  {
+    id: "06",
+    name: "Jack",
+    img: "/src/adotar/img/petJack.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 2,
+    adotado: false,
+    sexo: "male",
+    tipo: "dog",
+  },
+  {
+    id: "07",
+    name: "Gilmar",
+    img: "/src/adotar/img/petGilmar.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 3,
+    adotado: false,
+    sexo: "male",
+    tipo: "cat",
+  },
+  {
+    id: "08",
+    name: "Gilmar",
+    img: "/src/adotar/img/petGilmar.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 3,
+    adotado: false,
+    sexo: "male",
+    tipo: "cat",
+  },
+  {
+    id: "05",
+    name: "Gilmar",
+    img: "/src/adotar/img/petGilmar.png",
+    description:
+      "Esse pet é uma doçura de fofo, além de ser muito educado e prestativo quando o assunto é deixar o seu dia mais feliz, não tenho dúvida disso. Por isso é com pesar que disponibilizo ele para adoção!",
+    idade: 3,
+    adotado: false,
+    sexo: "male",
+    tipo: "bird",
+  },
+];
+
+function irParaPet(id) {
+  if (typeof id === "string") {
+    console.log(id);
+    const pet = pets.filter((pet) => pet.id === id);
+    console.log(pet[0]);
+
+    localStorage.setItem("pet", JSON.stringify(pet[0]));
+  }
+
   window.location.href = "/src/pet/pet.html";
 }
 
-function count() {
-  setInterval(() => {
-    console.log(Date.now(), 1000);
+// let petsDom = {
+//   p1: document.getElementById("01"),
+//   p2: document.getElementById("02"),
+//   p3: document.getElementById("03"),
+//   p4: document.getElementById("04"),
+//   p5: document.getElementById("05"),
+//   p6: document.getElementById("06"),
+// };
+
+// console.log(petsDom);
+
+function filterBirds() {
+  const petsNoTypeBirds = pets.filter((pet) => pet.tipo != "bird");
+
+  let p = [];
+  for (pet of petsNoTypeBirds) {
+    if (typeof pet.id === "string") {
+      p.push(document.getElementById(pet.id));
+    }
+  }
+  console.log(p);
+  let pClassesCss = [];
+  p.map((pet) => {
+    if (!!pet) {
+      let pClassesCss = [...pet.classList];
+      if (pClassesCss.includes("remove") && !!pet) {
+        pet.classList.remove("remove");
+        return;
+      }
+      pet.classList.add("remove");
+    }
   });
 }
+
+function filterDogs() {}
+
+function filterCats() {}
+
+function filterFemale() {}
+
+function filterMale() {}
+
+function filterMore5Years() {}
+
+function filterMinus5Years() {}
