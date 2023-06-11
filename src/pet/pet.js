01573ec0e102e82d20442c84af09230adb97eeba
Adenilson-Oliveira <adenilsonnogueira3@gmail.com>
2023-06-11 11:08:34 -0300

    deixando a página de pet mais dinâmica

--- src/pet/pet.js
+++ src/pet/pet.js
@@ -1,9 +1,16 @@
 const petImg = document.getElementById("imgPet");
 const petDescription = document.getElementById("description");
+const petName = document.getElementById("name");
+const petIdade = document.getElementById("idade");
+const petSexo = document.getElementById("sexo");
 
 const petDataString = localStorage.getItem("pet");
 
 const petData = JSON.parse(petDataString);
 
 petImg.src = petData.img;
+petName.innerText = petData.sexo === 'male' ? 'Adote o ' + petData.name : ' Adote a ' + petData.name
+petIdade.innerText = petData.idade > 1 ? petData.idade + ' anos' : petData.idade + ' ano'
 petDescription.innerText = petData.description;
+petSexo.innerText = petData.sexo === 'male' ? 'Sexo : macho' : 'Sexo: fêmea'
+
