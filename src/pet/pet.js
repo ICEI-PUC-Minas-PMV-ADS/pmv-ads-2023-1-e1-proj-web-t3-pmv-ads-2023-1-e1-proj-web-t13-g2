902baf53fb669e51b6138c5f2bc8b3ca192a83ab
emsfilho <130935213+emsfilho@users.noreply.github.com>
2023-06-21 10:50:04 -0300

    Refatoração da página pet

--- src/pet/pet.js
+++ src/pet/pet.js
@@ -1,16 +1,28 @@
-const petImg = document.getElementById("imgPet");
-const petDescription = document.getElementById("description");
-const petName = document.getElementById("name");
-const petIdade = document.getElementById("idade");
-const petSexo = document.getElementById("sexo");
+const petPhoto = document.getElementById("petPhoto");
+const petName = document.getElementById("petName");
+const petAge = document.getElementById("petAge");
+const petDescription = document.getElementById("petDescription");
+const petSex = document.getElementById("petSex");
+let pet = resolvePetData();
 
-const petDataString = localStorage.getItem("pet");
+document.addEventListener("DOMContentLoaded", () => {
+  renderPetCard();
+})
 
-const petData = JSON.parse(petDataString);
+function renderPetCard() {
+  petPhoto.src = pet.img;
+  petName.innerText =
+  pet.sexo === "male"
+    ? "Adote o " + pet.name
+    : " Adote a " + pet.name;
+  petAge.innerText =
+  pet.idade > 1 ? pet.idade + " anos" : pet.idade + " ano";
+  petDescription.innerText = pet.description;
+  petSex.innerText = pet.sexo === "male" ? "Sexo : macho" : "Sexo: fêmea";
+}
 
-petImg.src = petData.img;
-petName.innerText = petData.sexo === 'male' ? 'Adote o ' + petData.name : ' Adote a ' + petData.name
-petIdade.innerText = petData.idade > 1 ? petData.idade + ' anos' : petData.idade + ' ano'
-petDescription.innerText = petData.description;
-petSexo.innerText = petData.sexo === 'male' ? 'Sexo : macho' : 'Sexo: fêmea'
+function resolvePetData() {
+  let localStoragePet = localStorage.getItem("pet");
+  return JSON.parse(localStoragePet);
+}
 
